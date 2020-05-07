9e967de56b4d369118ad3360598c868a1a59e99f
Amy Nguyen <43863813+AmyNguyen406@users.noreply.github.com>
2020-05-07 00:00:03 -0500

    Update DropTables.sql

--- DropTables.sql
+++ DropTables.sql
@@ -3,6 +3,6 @@
 	Used in the event that we need to redesign the schema. 
 */
 
---DROP TABLE IF EXISTS Department, Room, Employee, Patient, DepartmentRooms, EmployeePatient, Inpatient, MedicalRecord CASCADE;
+DROP TABLE IF EXISTS Department, Room, Employee, Patient, DepartmentRooms, EmployeePatient, Inpatient, MedicalRecord CASCADE;
 
-SELECT * FROM DEPARTMENT;
+
